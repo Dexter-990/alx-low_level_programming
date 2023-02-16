2596fa453d10818383b9a4515d749678b743072f
Dexter <dexter.dev17@gmail.com>
2023-02-16 15:32:32 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-int main()
+
+/*Prints out a string using puts*/
+int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
-	return 0;
+	return (0);
 }
