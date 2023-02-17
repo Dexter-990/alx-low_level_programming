4a9fe332989627beaf03345d6fb124e991470d36
Dexter <dexter.dev17@gmail.com>
2023-02-16 16:03:00 -0800

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-/*Prints out a string using puts*/
+/**
+ * main - Prints Programming is like building a multilingual puzzle
+ *
+ * Description: Sets char as data type of str and makes it an array
+ * section header: dajnca jnaslnlasnknas
+ * Return: returns 0
+ */
+
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
