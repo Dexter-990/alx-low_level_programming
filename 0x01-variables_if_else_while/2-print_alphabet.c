595f5608aca4c946f8a0427f505da9e4f7868f88
Dexter <dexter.dev17@gmail.com>
2023-02-17 16:25:23 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints out a-z using putchar and a loop
+ *
+ * Return: returns 0 if success, else nonzero
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 97; c != 122; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
