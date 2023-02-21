6bd36ca751a8b9c6462c5688c2b6897ecc51857f
Dexter <dexter.dev17@gmail.com>
2023-02-21 15:53:59 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+/**
+ * main - prints _putchar
+ *
+ * @s: The character(s) to be printed
+ * Return: returns 0 for success
+ */
+int puts(const char *s);
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
