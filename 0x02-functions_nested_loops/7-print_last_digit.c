15314ff2dbbb39e8d9c27451b4844d5c319a800d
Dexter <dexter.dev17@gmail.com>
2023-02-22 07:36:50 -0800

    Update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,12 +12,11 @@ int print_last_digit(int n)
 	int x;
 	if (n >= 0)
 	{
-		x = n%10;
+		x = n % 10;
 	}
 	else
 	{
-		n = n * -1;
-		x = n%10;
+		x = (n % 10) *-1;
 	}
 	_putchar('0' + x);
 	return (x);
