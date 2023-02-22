3d76614204aacc05f347e7ff449e808851ea0351
Dexter <dexter.dev17@gmail.com>
2023-02-21 17:57:16 -0800

    Update

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if word is an alpha
+ *
+ * @c: contains character to be checked
+ * Return: returns 1 if true otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97) && (c <= 122) || (c >= 65) && (c <= 90))
+		return (1);
+	else
+		return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int _putchar(char ch);
 void print_alphabet(void);
 int _islower(int c);
 int putchar(int ch);
+int _isalpha(int c);
 void print_alphabet_x10(void);
 
 #endif
