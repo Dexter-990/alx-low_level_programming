3d3fc2de20ce914b24978f1c4f78d9d937b32b93
Dexter <dexter.dev17@gmail.com>
2023-02-21 16:12:37 -0800

    Update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
+#include "main.h"
 /**
  * main - prints _putchar
  *
- * @s: The character(s) to be printed
  * Return: returns 0 for success
  */
-int puts(const char *s);
 int main(void)
 {
 	puts("_putchar");
