025d3229d3fcadede7cf490cadf4562517cc77bf
Dexter <dexter.dev17@gmail.com>
2023-02-23 10:37:07 -0800

    Update

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -47,21 +47,3 @@ void print_remaining_days(int month, int day, int year)
 		}
 	}
 }
-int main(void)
-{
-	int month;
-	int day;
-	int year;
-
-	month = 2;
-	day = 29;
-	year = 2020;
-
-	printf("Date: %02d/%02d/%04d\n", month, day, year);
-
-	day = convert_day(month, day);
-
-	print_remaining_days(month, day, year);
-
-	return (0);
-}
