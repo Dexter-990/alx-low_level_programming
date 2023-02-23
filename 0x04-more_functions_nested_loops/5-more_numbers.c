105a8dffb5d4a25b23bd1905b8b934797af59c85
Dexter <dexter.dev17@gmail.com>
2023-02-23 14:01:41 -0800

    Update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14 te times
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+			else
+			{
+				_putchar(i + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
