da7d1a6545d97afc43434611a71d1aa019011383
Dexter <dexter.dev17@gmail.com>
2023-02-23 07:04:07 -0800

    Update

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test function that checks if integer is negative or positive
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 0;
+	negative_or_positive(i);
+	return (0);
+}
