877ffedcf3f0dd69b9c3118c0106a78afcfd8682
Dexter <dexter.dev17@gmail.com>
2023-02-27 10:50:07 -0800

    Reserve

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_rev - reserves a string
+ *
+ * @s: string to be reversed
+ */
+
+
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	int i;
+	char str[1000] = "";
+
+	for (i = 0; s[i]; i++)
+	{
+		str[i] = s[len - 1];
+		len--;
+	}
+	strcpy(s, str);
+}
+
