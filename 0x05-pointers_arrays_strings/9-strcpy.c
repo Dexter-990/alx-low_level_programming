825138064bd58640ec9331aad265c1d4bd0a180b
Dexter <dexter.dev17@gmail.com>
2023-02-28 07:24:03 -0800

    Strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcpy - copies a string from one variable to another
+ *
+ * @des: variable to be copied to
+ * @src: variable to be copied from
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int len = strlen(src);
+
+	for (i = 0; i < len + 1; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+		
