7f869f7435bc89370465cd4555fda06064c00293
Dexter <dexter.dev17@gmail.com>
2023-03-01 11:52:35 -0800

    Update

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: string to be concatenated to
+ * @src: string to be concatenated from
+ * Return: returns the pointer dest
+ */
+
+char _strcat(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; dest[i]; i++)
+	{	
+
+}
