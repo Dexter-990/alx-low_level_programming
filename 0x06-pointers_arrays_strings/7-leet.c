a41c09bc3ec18a6bda59801f50a22753775d4c97
Dexter <dexter.dev17@gmail.com>
2023-03-08 11:57:37 -0800

    Update

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,20 +9,19 @@
 
 char *leet(char *str)
 {
-	int i;
+	int i, j;
+	char uncoded[11] = "aAeEoOtTlL";
+	char coded[11] = "4433007711";
 
 	for (i = 0; str[i]; i++)
 	{
-		if (str[i] == 'A' || str[i] == 'a')
-			str[i] = '4';
-		else if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		else if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		else if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		else if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
+		for (j = 0; uncoded[j]; j++)
+		{
+			if (str[i] == uncoded[j])
+			{
+				str[i] = coded[j];
+			}
+		}
 	}
 	return (str);
 }
