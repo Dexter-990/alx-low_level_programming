65647c935e1e23c0f73abad8fb6cae2d60b4e821
Dexter <dexter.dev17@gmail.com>
2023-06-12 11:12:39 -0700

    Done

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if word is palidrome
+ * @s: string to be checked
+ *
+ * Return: 1 if a string is palindrome 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+
+
