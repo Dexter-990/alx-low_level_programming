6b147e77023952a8d6943b75b6b75da55dea445e
Dexter <dexter.dev17@gmail.com>
2023-06-13 14:04:47 +0000

    Done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - prints the name of the executable file
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: status code
+ */
+
+int main(__attribute__ ((unused)) int argc, char* argv[])
+{
+	int y;
+
+	char x[50];
+
+	strcpy(x, argv[0]);
+	for (y = 0; x[y]; y++)
+		_putchar(x[y]);
+	_putchar('\n');
+
+	return (0);
+}
+	
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is for my argc and argv tasks
