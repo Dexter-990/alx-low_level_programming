12461368f8b1b56da7f693c2bc1e4e775b3092a5
Dexter <dexter.dev17@gmail.com>
2023-06-13 09:47:38 +0000

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -8,14 +8,11 @@
 
 int _isalpha(int c)
 {
-		if (c >= 65 && c <= 90)
-				{
-							return (1);
-								}
-			else
-					{
-								return (0);
-									}
-
-				return (c);
+	int _isalpha(int c)
+	{
+			if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+						return (1);
+				else
+							return (0);
+	}
 }
