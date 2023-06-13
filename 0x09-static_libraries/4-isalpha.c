19efa2e4d9ecc08cdeaa26e28992242343eb196d
Dexter <dexter.dev17@gmail.com>
2023-06-13 11:22:20 +0000

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -4,15 +4,12 @@
  *  * _isalpha - checks alphabetic character
  *   * @c: character argument
  *    * Return: 1 if character and 0 otherwise
- *     */
+ */
 
 int _isalpha(int c)
 {
-	int _isalpha(int c)
-	{
-			if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-						return (1);
-				else
-							return (0);
-	}
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
 }
