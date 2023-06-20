3e476c11d763b531ac0f10f41656a6ee627a79db
Dexter <dexter.dev17@gmail.com>
2023-06-20 20:59:36 +0000

    Done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,34 +14,16 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-
 	dog_t *d;
 	unsigned int x;
-/**
- * struct dog - Short description
- * @name: First member
- * @age: Second member
- * @owner: Third member
- *
- * Description: Longer description
- */
-	struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
 
 	d = malloc(sizeof(*d));
-	
 	if (d == 0)
 	{
 		free(d);
 		return (NULL);
 	}
-	
 	d->name = malloc(strlen(name) * sizeof(d->name));
-
 	if (d->name == NULL)
 	{
 		free(d->name);
@@ -49,12 +31,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	for (x = 0; name[x]; x++)
-	{
 		d->name[x] = name[x];
-	}
-	
 	d->age = age;
-
 	d->owner = malloc(strlen(owner) * sizeof(d->owner));
 	if (d->owner == NULL)
 	{
@@ -64,10 +42,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	for (x = 0; owner[x]; x++)
-	{
 		d->owner[x] = owner[x];
-	}
-
 	return (d);
 }
 
