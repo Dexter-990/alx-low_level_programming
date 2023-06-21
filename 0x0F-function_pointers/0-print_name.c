f7c519c8c868bac6e0dd913bb448c07c6ae17a9c
Dexter <dexter.dev17@gmail.com>
2023-06-21 17:00:09 +0000

    Name printed

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints any name using a call back function
+ *
+ * @name: name to be printed
+ * @f: pointer to function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
