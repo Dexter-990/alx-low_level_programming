5e2ea51844629d84d736b23ab56f238b3b7758a7
Dexter <dexter.dev17@gmail.com>
2023-07-06 01:29:24 +0000

    Done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,46 +13,40 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr = *head, *current, *add;
-	unsigned int x = 0, a;
+	unsigned int a;
 
-	if (head != NULL)
+	listint_t *new = malloc(sizeof(listint_t));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	if (new == NULL)
 	{
-		listint_t *new = malloc(sizeof(listint_t));
+		return (NULL);
+	}
 
-		if (new != NULL)
-		{
-			new->n = n;
-			new->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
-			if (idx == 0)
-			{
-				add = add_nodeint(&ptr, n);
-				return (add);
-			}
-			while (ptr != NULL)
-			{
-				x++;
-				ptr = ptr->next;
-			}
-			if (idx > x)
-			{
-				return (NULL);
-			}
-			ptr = *head;
-			for (a = 0; a < x; a++)
-			{
-				if (a == idx - 1)
-				{
-					current = ptr->next;
-					ptr->next = new;
-					new->next = current;
-					break;
-				}
-				ptr = ptr->next;
-			} return (new);
-		} return (NULL);
-	} return (NULL);
+	if (idx == 0)
+	{
+		add = add_nodeint(&ptr, n);
+		return (add);
+	}
+
+	for (a = 0; a < (idx - 1); a++)
+	{
+		if (ptr == NULL)
+			return (NULL);
+		ptr = ptr->next;
+	}
+	current = ptr->next;
+	ptr->next = new;
+	new->next = current;
+
+	return (new);
 }
+
 
 
 /**
