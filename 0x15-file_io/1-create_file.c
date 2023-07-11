a6668161dc48247e5076333a0c12ebee76315eb5
Dexter <dexter.dev17@gmail.com>
2023-07-11 01:32:26 +0000

    Done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,10 +14,11 @@ int create_file(const char *filename, char *text_content)
 	if (!filename || !text_content)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	fd = open(filename, O_TRUNC | O_WRONLY | O_CREAT, 0600);
 	if (fd < 0)
 		return (-1);
 	w = write(fd, text_content, strlen(text_content));
+	close(fd);
 	if (w < 0)
 		return (-1);
 	return (1);
