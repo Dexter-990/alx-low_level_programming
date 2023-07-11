9313f96bd18a38d9dc1b0e6cef9e92fcd53cad3d
Dexter <dexter.dev17@gmail.com>
2023-07-11 02:09:03 +0000

    Done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	w = write(fd, text_content, 1);
+	w = write(fd, text_content, strlen(text_content));
 	if (w < 0)
 		return (-1);
 	return (1);
