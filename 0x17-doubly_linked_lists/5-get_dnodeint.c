eab56d726d29563a113b53e0ee0cb27385282f85
Dexter <dexter.dev17@gmail.com>
2023-08-18 21:10:52 +0000

    get node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (NULL);
 	ptr = head;
 
-	while (ptr->next != NULL)
+	while (ptr != NULL)
 	{
 		if (count == index)
 			return (ptr);
