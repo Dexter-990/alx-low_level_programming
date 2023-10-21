ea91de9e62ba1ad252905981167ceb1e1162013c
Dexter <dexter.dev17@gmail.com>
2023-10-21 07:28:32 +0000

    done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,4 +10,12 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void copier(char *src, char *dest);
 void _printerror(int status, char *filename);
+void closeElf(int elf);
+void printType(unsigned int e_type, unsigned char *e_ident);
+void printEntry(unsigned long int e_entry, unsigned char *e_ident);
+void printAbi(unsigned char *e_ident);
+void printVersion(unsigned char *e_ident);
+void printData(unsigned char *e_ident);
+void printMagic(unsigned char *e_ident);
+void checkElf(unsigned char *e_ident);
 #endif
